cbd7d2ad015e085aba58ecb31018c1226f38a17c
mpolive <mpolive@gmail.com>
2023-05-26 12:04:12 -0300

    add new commands

---
+++ primeiro.py
@@ -0,0 +1,12 @@
+print('Hello World')
+
+idade = 45
+altura = 1.74
+nome = 'Marcos Paulo de Oliveira'
+
+print('idade:', idade)
+print('Altura:', altura)
+print('Nome:', nome )
+estudando = True
+
+print(type(estudando))
